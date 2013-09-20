248f62fd132ae1d8d3bf7d513590438af2ff71fe
cfinkelstein <conrad.finkelstein@cofinpro.de>
2013-09-20 15:26:48 +0200

    Initiales WAR-Archiv

---
+++ bierdeckel_service/src/main/groovy/de/cofinpro/bierdeckel/repositories/GroovyTest.groovy
@@ -0,0 +1,11 @@
+package de.cofinpro.bierdeckel.repositories
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cfinkelstein
+ * Date: 20.09.13
+ * Time: 15:22
+ * To change this template use File | Settings | File Templates.
+ */
+class GroovyTest {
+}
