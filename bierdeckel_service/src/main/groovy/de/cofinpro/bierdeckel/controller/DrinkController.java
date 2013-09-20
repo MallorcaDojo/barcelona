8fa55b43f12c69b83d367c65858fb9c2f72ef792
heckm <michael.heck@cofinpro.de>
2013-09-20 19:10:44 +0200

    Multiple arguments for drink controller.

--- bierdeckel_service/src/main/groovy/de/cofinpro/bierdeckel/controller/DrinkController.java
+++ bierdeckel_service/src/main/groovy/de/cofinpro/bierdeckel/controller/DrinkController.java
@@ -2,7 +2,10 @@ package de.cofinpro.bierdeckel.controller;
 
 import de.cofinpro.bierdeckel.domain.Drink;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,12 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class DrinkController {
 
     @ResponseBody
-    @RequestMapping(value = "/drinks/{name}", method = RequestMethod.GET)
-    public Drink drinks(@PathVariable("name") String name) {
-        System.out.println("Hallo Welt");
+    @RequestMapping(value = "/drinks/{name}/{price}", method = RequestMethod.GET)
+    public Drink drinks(@PathVariable String name, @PathVariable String price) {
+        Double priceAsDouble;
+        priceAsDouble = Double.parseDouble(price);
 
         Drink drink = new Drink();
         drink.setName(name);
+        drink.setPrice(priceAsDouble);
+
         return drink;
     }
 
