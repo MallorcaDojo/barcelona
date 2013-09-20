7129bc7a45650a7a05bec960c98db1bb130eefac
cfinkelstein <conrad.finkelstein@cofinpro.de>
2013-09-20 16:13:02 +0200

    Initiales WAR-Archiv

---
+++ bierdeckel_service/src/test/de/cofinpro/bierdeckel/repostories/SampleTest.java
@@ -0,0 +1,28 @@
+package de.cofinpro.bierdeckel.repostories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cfinkelstein
+ * Date: 20.09.13
+ * Time: 16:02
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:dispatcher-servlet.xml"})
+public class SampleTest {
+
+    @Test
+    public void myTest() {
+         assertTrue(true);
+    }
+
+
+}
