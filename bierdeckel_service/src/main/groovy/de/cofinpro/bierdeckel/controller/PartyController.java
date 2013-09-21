c73abbfdacc2a24a43a9be79c7762f59d9ca082c
cfinkelstein <conrad.finkelstein@cofinpro.de>
2013-09-21 11:15:23 +0200

    MockController

--- bierdeckel_service/src/main/groovy/de/cofinpro/bierdeckel/controller/PartyController.java
+++ bierdeckel_service/src/main/groovy/de/cofinpro/bierdeckel/controller/PartyController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,22 +27,94 @@ public class PartyController implements PartyDao {
     @ResponseBody
     @RequestMapping(value = "/parties", method = RequestMethod.POST)
     public Party addParty(@RequestBody DonnerBuddy buddy, @RequestBody Party party)  {
-       return null;
+        buddy.setId("12345");
+        buddy.setCreated(new Date());
+        buddy.setUpdated(new Date());
+        buddy.setVersion(0l);
+
+        party.setVersion(0l);
+        party.setUpdated(new Date());
+        party.setCreated(new Date());
+        party.setDonnerBuddies(new ArrayList<DonnerBuddy>());
+        party.getDonnerBuddies().add(buddy);
+        party.setId("12345");
+
+       return party;
     }
+
+
+    private static DonnerBuddy getDonnerBuddy(String id) {
+        DonnerBuddy donnerBuddy = new DonnerBuddy();
+        donnerBuddy.setName("asdlajsdl" + id);
+        donnerBuddy.setId(id);
+        donnerBuddy.setUpdated(new Date());
+        donnerBuddy.setCreated(new Date());
+        donnerBuddy.setVersion(0l);
+        return donnerBuddy;
+    }
+
+    private static Party getParty(String id) {
+        Party party = new Party();
+        party.setId(id);
+        party.setName("sfsdf" + id);
+        party.setUpdated(new Date());
+        party.setCreated(new Date());
+        party.setDonnerBuddies(new ArrayList<DonnerBuddy>());
+        party.setDrinks(new ArrayList<Drink>());
+
+        for(int i=0;i<10;i++) {
+            DonnerBuddy donnerBuddy = getDonnerBuddy(String.valueOf(i));
+            party.getDonnerBuddies().add(donnerBuddy);
+        }
+
+        int i=0;
+        for(DonnerBuddy donnerBuddy: party.getDonnerBuddies()) {
+            Drink drink = getDrink(String.valueOf(i));
+            drink.setDonnerBuddy(donnerBuddy);
+            party.getDrinks().add(drink);
+            i++;
+        }
+
+        return party;
+    }
+
+    private static Drink getDrink(String id) {
+        Drink drink = new Drink();
+        drink.setName("adfsdf");
+        drink.setUpdated(new Date());
+        drink.setCreated(new Date());
+        drink.setVersion(0l);
+
+
+
+
+        return drink;
+    }
+
 
 
     @Override
     @ResponseBody
     @RequestMapping(value = "/parties/{buddyId}", method = RequestMethod.GET)
     public List<Party> showParties(@PathVariable String buddyId) {
-      return null;
+
+       List<Party> parties = new ArrayList<Party>();
+       for(int i=0;i<10;i++) {
+          parties.add(getParty(String.valueOf(i)));
+       }
+
+
+      return parties;
     }
 
     @Override
     @ResponseBody
     @RequestMapping(value = "/parties/{partyId}", method = RequestMethod.POST)
     public DonnerBuddy addDonnerBuddy(@RequestBody DonnerBuddy buddy, @PathVariable String partyId)  {
-      return null;
+        Party party = getParty(partyId);
+        DonnerBuddy donnerBuddy = getDonnerBuddy("445");
+        party.getDonnerBuddies().add(buddy);
+        return donnerBuddy;
     }
 
     @Override
@@ -55,12 +128,13 @@ public class PartyController implements PartyDao {
     @ResponseBody
     @RequestMapping(value = "/drinks/{partyId}", method = RequestMethod.GET)
     public List<Drink> showDrinks(@PathVariable String partyId) {
-        return null;
-    }
+        List<Drink> drinks = new ArrayList<Drink>();
+        for(int i=0;i<10;i++) {
+            Drink drink = getDrink(String.valueOf(i));
+            drink.setDonnerBuddy(getDonnerBuddy(String.valueOf(i)));
+            drinks.add(drink);
+        }
 
-    public Bill createBill(String partyId) {
-        Bill b = new Bill();
-
-        return b;
+        return drinks;
     }
 }
